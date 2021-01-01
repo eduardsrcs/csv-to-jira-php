84fa2a6f14388a0f7c6284f897722c5fce91ee47
eduardsrcs <itbeard@inbox.lv>
2021-01-01 18:21:01 +0200

    checking MIME types

--- csvparser.php
+++ csvparser.php
@@ -16,6 +16,16 @@ if(isset($_POST['import'])) {
     if(isset($csv['tmp_name']) && !empty($csv['tmp_name'])) {
 
       //
+      $finfo = finfo_open(FILEINFO_MIME_TYPE);
+      $mime = finfo_file($finfo, $csv['tmp_name']);
+      finfo_close($finfo);
+
+      $allowed_mime = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+      //
+      if(in_array($mime, $allowed_mime) && is_uploaded_file($csv['tmp_name'])) {
+        //
+        echo 'MIME type passes<br>';
+      }
       //                                           
     }
   }
