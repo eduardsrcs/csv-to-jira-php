aa6ecc4dcf2b599da0ac549e624daf76c598c5cc
eduardsrcs <itbeard@inbox.lv>
2021-01-01 17:32:00 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>CSV to jira parser form</title>
+  
+</head>
+<body>
+  <h1>CSV to JIRA parsing</h1>
+  <form method="POST" action="csvparser.php">
+    <input type="file">
+    <input type="submit" value="submit">
+  </form>
+</body>
+</html>
+<?php
+  
+?>
